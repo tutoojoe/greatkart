dc7625b5009930399d00f7033a35dd8c79e86bbb
tutoojoe <tutoojoephilip@gmail.com>
2021-11-16 17:25:40 +0530

    checking the styles

--- greatkart/views.py
+++ greatkart/views.py
@@ -41,14 +41,14 @@ def home(request):
             prod_offer = ProductOffer.objects.get(product_id = product, valid_from__lte=now, valid_to__gte=now, is_active = True)
             p_offer = prod_offer.discount
             
-            print(p_offer.discount,'for',product)
+            
         except:
             pass
         
         try:
             categ_offer = CategoryOffer.objects.get(category_id = product.category,valid_from__lte=now, valid_to__gte=now, is_active = True)
             c_offer = categ_offer.discount
-            print(c_offer.discount,'for',product,'- is the product and ',product.category)
+            
         except:
             pass
 
@@ -56,22 +56,19 @@ def home(request):
             disc_price = product.mrp_price - (product.mrp_price * p_offer)/100
             product.price = round(disc_price, 2)
             product.save()
-            print(disc_price, 'product - ',product, ' this will be the new price based on product offer. Old price was - ',product.mrp_price)
-            # print(product,' - ', p_offer.discount,'product offer is more')
         elif p_offer < c_offer:
             disc_price = product.mrp_price - (product.mrp_price * c_offer)/100
             product.price = disc_price
             product.save()
-            print(disc_price, 'product - ',product,' this will be the new price based on CATEGORY. Old price was - ',product.mrp_price)
-            # print(c_offer.discount,' - ', 'category offer is more')
+            
         elif p_offer == c_offer and p_offer !=0 :
-            print('here both the offers are same for this product', product)
+       
             disc_price = product.mrp_price - (product.mrp_price * p_offer)/100
             product.price = disc_price
             product.save()
 
         else:
-            print(product, ' - this one has no offer')
+           pass
         
     reviews = ReviewRating.objects.filter(product_id = prod_id, status = True)
 
@@ -84,16 +81,14 @@ def home(request):
     return render(request, 'home.html',context)
 
 def images(request):
-    print('request received')
+
     if request.method == "POST":
         form = ProductForm(request.POST or None, request.FILES or None)
 
         if form.is_valid():
-            print('Form Valid')
-            print('form valid')
             product_name = form.cleaned_data['product_name']
             slug = slugify(product_name)
-            print(slug)
+         
             description = form.cleaned_data['description']
             price = form.cleaned_data['price']
             
@@ -107,7 +102,7 @@ def images(request):
             
             return redirect('images')
     else:
-        print('Form Valid')
+        
         form = ProductForm()
     context     = {
             'form' : form,
