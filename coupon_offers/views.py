43b35fbfd16949b3b529ede16d2f7af48946215d
tutoojoe <tutoojoephilip@gmail.com>
2021-11-24 20:38:57 +0530

    coupon part view edited. Admin side show nett paid value in the case of any coupon redeem

--- coupon_offers/views.py
+++ coupon_offers/views.py
@@ -40,18 +40,55 @@ def verify_coupon(request):
             # print('checking coupon')
             coupon = Coupon.objects.get(code__iexact = code, valid_from__lte=now, valid_to__gte=now, active = True)
             if coupon:
+                # print('coupon valid')
+
                 try:
                     coupon_already_used = Order.objects.get(user=request.user,coupon=coupon,coupon_use_status=True)
-                    print(coupon_already_used,"coupon not available")
+                    # print(coupon_already_used,"coupon not available")
                     if coupon_already_used:
-                        print("coupon used")
-                        print("coupon already used")
+                        # print("coupon used")
+                        # print("coupon already used")
                         context = {
                         "success":"coupon already used",
                         }
                         return JsonResponse (context)
                     else:
-                        print('program runs this',coupon)
+                        # print('program runs this',coupon)
+                        discount = coupon.discount
+                        # print(discount)
+                        order = Order.objects.get(user = request.user, is_ordered = False)
+                        order_no = order.order_number
+                        order.coupon = coupon
+                        order.discount = round(discount,2)
+                        order.save()
+                        # print(order_no)
+                        # print('got order')
+                        current_user = request.user
+                        cart_items = CartItem.objects.filter(user = current_user)
+                        grand_total = 0
+                        tax = 0
+                        total = 0
+                        quantity = 0
+                        for cart_item in cart_items:
+                            total   += (cart_item.product.price * cart_item.quantity)
+                            quantity += cart_item.quantity
+                        tax = round((5 * total)/100,2)
+                        grand_total = round(total + tax,2)
+                    
+                        discount_amount = round(grand_total * discount/100,2)
+                        # print(discount_amount,'discount amount')
+                        total_after_coupon = round(float(grand_total - discount_amount),2)
+                        # print(grand_total,'total')
+                        # print(total_after_coupon,'amount after discount')
+
+                        context = {
+                            "success":"valid",
+                            "discount_amount": discount_amount,
+                            "total_after_coupon":total_after_coupon,
+                        }
+                        return JsonResponse(context)
+                        
+                except:
                     discount = coupon.discount
                     # print(discount)
                     order = Order.objects.get(user = request.user, is_ordered = False)
@@ -84,11 +121,8 @@ def verify_coupon(request):
                         "discount_amount": discount_amount,
                         "total_after_coupon":total_after_coupon,
                     }
-                    return JsonResponse(context)
-                        
-                except:                    
-                    pass
-
+                    return JsonResponse(context)                    
+                    
         except Coupon.DoesNotExist:
             # print('no coupon available')
             context = {
